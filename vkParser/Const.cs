cf1281d470ef54ddddd4aa81babf2f6a8cf65933
alexandr-galochkin <43029653+alexandr-galochkin@users.noreply.github.com>
2018-12-10 22:03:52 +0300

    v3.0

--- vkParser/Const.cs
+++ vkParser/Const.cs
@@ -8,12 +8,16 @@ namespace vkParser
 {
     class Const
     {
-        public const string command = @"C:\Phyton37\Scripts\vktop.exe";
-        public const string s1 = "https://vk.com/ru9gag";
-        public const string s2 = "https://vk.com/amfet1";
-        public const string s3 = "https://vk.com/pixel_memes";
-        public const string s4 = "https://vk.com/greatmem";
-        public const string s5 = "https://vk.com/dobriememes";
-        public const string s6 = "https://vk.com/mudakoff";
+        public const string command = @"C:\Users\Александр\AppData\Local\Programs\Python\Python37-32\Scripts\vktop.exe";
+        public const string s1 = "https://vk.com/greatmem";
+        public const string s2 = "https://vk.com/pixel_memes";
+        public const string s3 = "https://vk.com/amfet1";
+        public const string s4 = "https://vk.com/mudakoff";
+        public const string s5 = "https://vk.com/abstract_memes";
+        public const string s6 = "https://vk.com/inscriptions_on_pics";
+        public const string s7 = "https://vk.com/darknet_nash";
+        public const string s8 = "https://vk.com/4ch";
+        public const string s9 = "https://vk.com/zloyshkolnik";
+        public const string s10 = "https://vk.com/desu4nya";
     }
 }
--- vkParser/Program.cs
+++ vkParser/Program.cs
@@ -14,16 +14,30 @@ namespace vkParser
     {
         static void Main(string[] args)
         {
-            string pathToSave = "C:\\Users\\fzhil\\Downloads\\vk\\";
+            string pathToSave0 = "C:\\Users\\Александр\\Desktop\\Мемы_Приколы\\";
+            string pathToSave1 = "C:\\Users\\Александр\\Desktop\\мемная_папка\\";
+            string pathToSave2 = "C:\\Users\\Александр\\Desktop\\МЕМЫ\\";
+            string pathToSave3 = "C:\\Users\\Александр\\Desktop\\MDK\\";
+            string pathToSave4 = "C:\\Users\\Александр\\Desktop\\абстрактные_мемы_для_элиты_всех_сортов\\";
+            string pathToSave5 = "C:\\Users\\Александр\\Desktop\\сомнительные_картинки_и_подписи_к_ним\\";
+            string pathToSave6 = "C:\\Users\\Александр\\Desktop\\Даркнет_который_мы_заслужили\\";
+            string pathToSave7 = "C:\\Users\\Александр\\Desktop\\4ch\\";
+            string pathToSave8 = "C:\\Users\\Александр\\Desktop\\ЗШ\\";
+            string pathToSave9 = "C:\\Users\\Александр\\Desktop\\DESUCHAN\\";            
+
             string content = "";
 
-            //          паблик     топ 10  (.)  дней назад (10)
-            string arg1 = Const.s1 + " --top 2 -w 8 -d 10";
-            string arg2 = Const.s2 + " --top 2 -w 8 -d 10";
-            string arg3 = Const.s3 + " --top 2 -w 8 -d 10";
-            string arg4 = Const.s4 + " --top 2 -w 8 -d 10";
-            string arg5 = Const.s5 + " --top 2 -w 8 -d 10";
-            string arg6 = Const.s6 + " --top 2 -w 8 -f 01.10.2018";
+            //          паблик     топ 50  (.)  дней назад (100)
+            string arg1 = Const.s1 + " --top 50 -w 8 -d 100";
+            string arg2 = Const.s2 + " --top 50 -w 8 -d 100";
+            string arg3 = Const.s3 + " --top 50 -w 8 -d 100";
+            string arg4 = Const.s4 + " --top 50 -w 8 -d 100";
+            string arg5 = Const.s5 + " --top 50 -w 8 -d 100";
+            string arg6 = Const.s6 + " --top 50 -w 8 -d 100";
+            string arg7 = Const.s7 + " --top 50 -w 8 -d 100";
+            string arg8 = Const.s8 + " --top 50 -w 8 -d 100";
+            string arg9 = Const.s9 + " --top 50 -w 8 -d 100";
+            string arg10 = Const.s10 + "--top 50 -w 8 -d 100";
 
             Task task1 = Task.Run(() =>
             {
@@ -48,8 +62,9 @@ namespace vkParser
 
                 // закрываем процесс
                 procCommand.WaitForExit();
-            });
+                Console.WriteLine(content);
 
+            });         
 
             Task task2 = task1.ContinueWith(ant =>
             {
@@ -62,6 +77,7 @@ namespace vkParser
                 Process procCommand = Process.Start(psiOpt);
                 StreamReader srIncoming = procCommand.StandardOutput;
                 content += srIncoming.ReadToEnd();
+                Console.WriteLine(content);
                 procCommand.WaitForExit();
             });
 
@@ -77,6 +93,8 @@ namespace vkParser
                 StreamReader srIncoming = procCommand.StandardOutput;
                 content += srIncoming.ReadToEnd();
                 procCommand.WaitForExit();
+                Console.WriteLine(content);
+
             });
 
             Task task4 = task3.ContinueWith(ant =>
@@ -90,6 +108,8 @@ namespace vkParser
                 Process procCommand = Process.Start(psiOpt);
                 StreamReader srIncoming = procCommand.StandardOutput;
                 content += srIncoming.ReadToEnd();
+                Console.WriteLine(content);
+
                 procCommand.WaitForExit();
             });
 
@@ -104,6 +124,8 @@ namespace vkParser
                 Process procCommand = Process.Start(psiOpt);
                 StreamReader srIncoming = procCommand.StandardOutput;
                 content += srIncoming.ReadToEnd();
+                Console.WriteLine(content);
+
                 procCommand.WaitForExit();
             });
 
@@ -118,21 +140,107 @@ namespace vkParser
                 Process procCommand = Process.Start(psiOpt);
                 StreamReader srIncoming = procCommand.StandardOutput;
                 content += srIncoming.ReadToEnd();
+                Console.WriteLine(content);
+
                 procCommand.WaitForExit();
             });
 
-            Task finalTask = Task.Factory.ContinueWhenAll(new Task[] { task1, task2, task3, task4, task5, task6 }, ant =>
+            Task task7 = task6.ContinueWith(ant =>
+            {
+                Console.WriteLine("7");
+                ProcessStartInfo psiOpt = new ProcessStartInfo(Const.command, arg7);
+                psiOpt.WindowStyle = ProcessWindowStyle.Hidden;
+                psiOpt.RedirectStandardOutput = true;
+                psiOpt.UseShellExecute = false;
+                psiOpt.CreateNoWindow = true;
+                Process procCommand = Process.Start(psiOpt);
+                StreamReader srIncoming = procCommand.StandardOutput;
+                content += srIncoming.ReadToEnd();
+                Console.WriteLine(content);
+
+                procCommand.WaitForExit();
+            });
+
+            Task task8 = task7.ContinueWith(ant =>
+            {
+                Console.WriteLine("8");
+                ProcessStartInfo psiOpt = new ProcessStartInfo(Const.command, arg8);
+                psiOpt.WindowStyle = ProcessWindowStyle.Hidden;
+                psiOpt.RedirectStandardOutput = true;
+                psiOpt.UseShellExecute = false;
+                psiOpt.CreateNoWindow = true;
+                Process procCommand = Process.Start(psiOpt);
+                StreamReader srIncoming = procCommand.StandardOutput;
+                content += srIncoming.ReadToEnd();
+                Console.WriteLine(content);
+
+                procCommand.WaitForExit();
+            });
+
+            Task task9 = task8.ContinueWith(ant =>
+            {
+                Console.WriteLine("9");
+                ProcessStartInfo psiOpt = new ProcessStartInfo(Const.command, arg9);
+                psiOpt.WindowStyle = ProcessWindowStyle.Hidden;
+                psiOpt.RedirectStandardOutput = true;
+                psiOpt.UseShellExecute = false;
+                psiOpt.CreateNoWindow = true;
+                Process procCommand = Process.Start(psiOpt);
+                StreamReader srIncoming = procCommand.StandardOutput;
+                content += srIncoming.ReadToEnd();
+                Console.WriteLine(content);
+
+                procCommand.WaitForExit();
+            });
+
+            Task task10 = task9.ContinueWith(ant =>
+            {
+                Console.WriteLine("10");
+                ProcessStartInfo psiOpt = new ProcessStartInfo(Const.command, arg10);
+                psiOpt.WindowStyle = ProcessWindowStyle.Hidden;
+                psiOpt.RedirectStandardOutput = true;
+                psiOpt.UseShellExecute = false;
+                psiOpt.CreateNoWindow = true;
+                Process procCommand = Process.Start(psiOpt);
+                StreamReader srIncoming = procCommand.StandardOutput;
+                content += srIncoming.ReadToEnd();
+                Console.WriteLine(content);
+
+                procCommand.WaitForExit();
+            });
+
+
+            Task finalTask = Task.Factory.ContinueWhenAll(new Task[] { task1, task2, task3, task4, task5, task6, task7, task8, task9, task10 }, ant =>
             {
                 int i = 0;
-                Console.WriteLine(content);
+               // Console.WriteLine(content);
                 List<string> ListURLImages = Parser_Images_From_Posts(content);
 
                 using (WebClient client = new WebClient())
                 {
                     foreach (string image in ListURLImages)
                     {
+                       if (i/50 == 0)
+                        client.DownloadFile(image, pathToSave0 + (i % 50).ToString() + ".jpg");
+                        if (i / 50 == 1)
+                            client.DownloadFile(image, pathToSave1 + (i % 50).ToString() + ".jpg");
+                        if (i / 50 == 2)
+                            client.DownloadFile(image, pathToSave2 + (i % 50).ToString() + ".jpg");
+                        if (i / 50 == 3)
+                            client.DownloadFile(image, pathToSave3 + (i % 50).ToString() + ".jpg");
+                        if (i / 50 == 4)
+                            client.DownloadFile(image, pathToSave4 + (i % 50).ToString() + ".jpg");
+                        if (i / 50 == 5)
+                            client.DownloadFile(image, pathToSave5 + (i % 50).ToString() + ".jpg");
+                        if (i / 50 == 6)
+                            client.DownloadFile(image, pathToSave6 + (i % 50).ToString() + ".jpg");
+                        if (i / 50 == 7)
+                            client.DownloadFile(image, pathToSave7 + (i % 50).ToString() + ".jpg");
+                        if (i / 50 == 8)
+                            client.DownloadFile(image, pathToSave8 + (i % 50).ToString() + ".jpg");
+                        if (i / 50 == 9)
+                            client.DownloadFile(image, pathToSave9 + (i % 50).ToString() + ".jpg");
                         i++;
-                        client.DownloadFile(image, pathToSave + i.ToString() + ".jpg");
                     }
                 }
 
@@ -190,17 +298,26 @@ namespace vkParser
             separators[0] = "<a";
             separators[1] = "a>";
 
-            string[] Post_HTML_View = Post[0].InnerHtml.ToString().Split(separators, StringSplitOptions.RemoveEmptyEntries); //Картинка поста - первый по порядку див-класс (поэтому нулевой), картинки в коментах - [1],...,[n]
-
-            if (Post_HTML_View.Count() < 2) //Проверяем состоит ли пост из нескольких мемов
+            try
             {
-                string[] tempArray = Post_HTML_View[0].ToString().Split('(', ')');
-                List<string> ListURLImages = tempArray.ToList().FindAll(i => i.StartsWith("http") == true);
+                string[] Post_HTML_View = Post[0].InnerHtml.ToString().Split(separators, StringSplitOptions.RemoveEmptyEntries); //Картинка поста - первый по порядку див-класс (поэтому нулевой), картинки в коментах - [1],...,[n]
 
-                return ListURLImages[0]; //Возвращаем ссылку на картинку поста
+                if (Post_HTML_View.Count() < 2) //Проверяем состоит ли пост из нескольких мемов
+                {
+                    string[] tempArray = Post_HTML_View[0].ToString().Split('(', ')');
+                    List<string> ListURLImages = tempArray.ToList().FindAll(i => i.StartsWith("http") == true);
+
+                    return ListURLImages[0]; //Возвращаем ссылку на картинку поста
+                }
+                else
+                {
+                    return null;
+                }
             }
-            else
+            catch (Exception ex)
             {
+
+                Console.WriteLine(ex.ToString());
                 return null;
             }
 
